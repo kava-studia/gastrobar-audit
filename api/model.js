3d19f4dc0c8e2d002258ec284b6dc80394f0023c
kava <yukava25@gmail.com>
2026-08-19 15:28:33 +0300

    AR V8: serve layered final model

--- api/model.js
+++ api/model.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const fileId = '1-Wh2hg8ALPg8eo-oI29uXhH2E7o9bSSe';
+  const fileId = '18VTjIpCPQs2cF61tVc-NA3Wo-I3_KJiV';
   const url = `https://drive.usercontent.google.com/download?id=${fileId}&export=download&confirm=t`;
   try {
     const upstream = await fetch(url, { redirect: 'follow' });
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'model/gltf-binary');
     res.setHeader('Content-Length', String(data.length));
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=604800');
+    res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
     res.status(200).send(data);
   } catch (error) {
     console.error(error);
