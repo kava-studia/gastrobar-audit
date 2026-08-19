db2b76aa200f500d5b739d8c5ed3be9d86d796db
kava <yukava25@gmail.com>
2026-08-19 16:09:33 +0300

    AR V11: serve founder demo fake AR model

--- api/model.js
+++ api/model.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const fileId = '1ATm5gaeusekKr1lY0b35V8E3W5Pycsz7';
+  const fileId = '1ybmRVndoH1mLMxT25c8Qus4gJiXANsWV';
   const url = `https://drive.usercontent.google.com/download?id=${fileId}&export=download&confirm=t`;
   try {
     const upstream = await fetch(url, { redirect: 'follow' });
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'model/gltf-binary');
     res.setHeader('Content-Length', String(data.length));
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=86400');
+    res.setHeader('Cache-Control', 'public, s-maxage=300, stale-while-revalidate=3600');
     res.status(200).send(data);
   } catch (error) {
     console.error(error);
