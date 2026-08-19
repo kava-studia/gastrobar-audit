1ace3bf6e3be714df9a7522792f1b222cb76d570
kava <yukava25@gmail.com>
2026-08-19 15:40:45 +0300

    AR V9: serve separated layered dish model

--- api/model.js
+++ api/model.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const fileId = '18VTjIpCPQs2cF61tVc-NA3Wo-I3_KJiV';
+  const fileId = '1NZ0OC8G3OnQzOPkM4nX3em3OYUBG-c5d';
   const url = `https://drive.usercontent.google.com/download?id=${fileId}&export=download&confirm=t`;
   try {
     const upstream = await fetch(url, { redirect: 'follow' });
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'model/gltf-binary');
     res.setHeader('Content-Length', String(data.length));
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
+    res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=86400');
     res.status(200).send(data);
   } catch (error) {
     console.error(error);
